e74a0823f516ad81922396b757e42469e35857c8
Ershadul Hoque <ershadulhoque@gmail.com>
2019-07-02 18:47:18 +0600

    Add companies listing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import CompanyIndex from "./components/company/CompanyIndex";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/companies"} className="nav-link">
+                  Companies
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>{" "}
+        <br />
+        <h2>Welcome to Rewards CMS</h2> <br />
+        <Switch>
+          <Route exact path="/companies" component={CompanyIndex} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.headers.common['tenant'] = '5d19a7831fb4de5748ccd2bd'; //process.env.REACT_APP_TENANT_ID
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
